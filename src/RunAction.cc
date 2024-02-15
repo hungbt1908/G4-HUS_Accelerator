25145cf3ceadf3aa7d1fe513193c99bb143d1447
HungBT <hungbt1908@gmail.com>
2024-02-15 19:31:23 +0700

    add new print info methods

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -1,6 +1,7 @@
 #include "RunAction.hh"
 #include "RunActionMessenger.hh"
 #include "DetectorConstruction.hh"
+#include "PhysicsList.hh"
 #include "PrimaryGeneratorAction.hh"
 
 #include "G4ParticleGun.hh"
@@ -110,6 +111,10 @@ void RunAction::EndOfRunAction(const G4Run* run)
     // get user detector
     const auto detConstruction = static_cast<const DetectorConstruction*>(G4RunManager::GetRunManager()->GetUserDetectorConstruction());
     
+    // get user physics list
+    const auto physicsList = static_cast<const PhysicsList*>(G4RunManager::GetRunManager()->GetUserPhysicsList());
+
+
     // get primary generator
     const auto generatorAction = static_cast<const PrimaryGeneratorAction*>(G4RunManager::GetRunManager()->GetUserPrimaryGeneratorAction());
     const G4ParticleGun* particleGun = generatorAction->GetParticleGun();
@@ -117,50 +122,44 @@ void RunAction::EndOfRunAction(const G4Run* run)
     const G4double particleEnergy = generatorAction->GetGunEnergy();
     // G4cout << particleEnergy << G4endl;
 
-    // open text file to write run information
-    if (IsMaster())
+    // Print
+    if(IsMaster())
     {
-        // open text file to write run information
-        std::ofstream file("Geant4_out.txt");
-        //geant4OutputName.open("Geant4.out", std::ios::out);
-
-        file << "                              ==> GEANT4 OUTPUT REPORT <==                            " << "\n";
-        file << "  __________________________________ [ Job Title ] _________________________________  " << "\n";
-        file << " |                                                                                  | " << "\n";
-        file << " | Application for simulating Accelerator at HUS                                    | " << "\n";
-        file << " | -------------------------------------------------------------------------------- | " << "\n";
-        file << " | author: BUI Tien Hung                                                            | " << "\n";
-        file << " | email : hungbt1908@gmail.com                                                     | " << "\n";
-        file << " | adress: Institute for Nuclear Science and Technology (INST)                      | " << "\n";
-        file << " |__________________________________________________________________________________| " << "\n";
-        file << "                                                                                      " << "\n";
-        file << "                         Starting = " << beginTime;
-        file << "                         Ending   = " << endTime;
-        file << "                                                                                      " << "\n";
-        file << " >>>> Geometry Report                                                            <<<< " << "\n";
-        file << "   + Target: " << "MatName = "    << detConstruction->logicTarget->GetMaterial()->GetName()                        << "; "
-                                            << "Density = "    << detConstruction->logicTarget->GetMaterial()->GetDensity()/(g/cm3) << " g/cm3" << "; "
-                                            << "Components = " << detConstruction->logicTarget->GetMaterial()->GetNumberOfElements()            << "; "
-                         << "\n";
-        file << "   + Baselayer: " << "MatName = "    << detConstruction->logicBackLayer->GetMaterial()->GetName()                        << "; "
-                                               << "Density = "    << detConstruction->logicBackLayer->GetMaterial()->GetDensity()/(g/cm3) << " g/cm3" << "; "
-                                               << "Components = " << detConstruction->logicBackLayer->GetMaterial()->GetNumberOfElements()            << "; "
-
-             << "\n";
-        file << "                                                                                      " << "\n";
-
-        file << " >>>> Physics Report                                                             <<<< " << G4endl;
-
-        file << " >>>> Source Report                                                              <<<< " << G4endl; 
-        // file << particleEnergy << G4endl;
-
-        file << " >>>> Simulation Report                                                          <<<< " << "\n";
-        file << "  + number of worker threads: " << numberOfWorkerThreads << " threads" << "\n";
-        file << "  + beam on                 : " << totalEvents << " events" << "\n";
-        file << "  + total events            : " << nofEvents   << " events" << "\n";
-        file << "  + elapsed time            : " << (double(diffclock(end, begin)/1000)) << " seconds    " << G4endl;
-        file.close();
+      G4cout << "                                                                                      " << G4endl;
+      G4cout << "                                                                                      " << G4endl;
+      G4cout << "                              ==> GEANT4 OUTPUT REPORT <==                            " << G4endl;
+      G4cout << "  __________________________________ [ Job Title ] _________________________________  " << G4endl;
+      G4cout << " |                                                                                  | " << G4endl;
+      G4cout << " | Application for simulating Accelerator at HUS                                    | " << G4endl;
+      G4cout << " | -------------------------------------------------------------------------------- | " << G4endl;
+      G4cout << " | author: BUI Tien Hung                                                            | " << G4endl;
+      G4cout << " | email : hungbt1908@gmail.com                                                     | " << G4endl;
+      G4cout << " | adress: Institute for Nuclear Science and Technology (INST)                      | " << G4endl;
+      G4cout << " |__________________________________________________________________________________| " << G4endl;
+      G4cout << "                                                                                      " << G4endl;
+      G4cout << "                         Starting = " << beginTime                                      << G4endl;
+      G4cout << "                         Ending   = " << endTime                                        << G4endl;
+      G4cout << "                                                                                      " << G4endl;
+      G4cout << " # Geometry setups:                                                                   " << G4endl;
+      G4cout << "   Target MatName         : " << detConstruction->logicTarget->GetMaterial()->GetName() << G4endl;
+      G4cout << "   Base Layer MatName     : " << detConstruction->logicBackLayer->GetMaterial()->GetName()<< G4endl;
+      G4cout << "   Target Angle           :
+       "
+      G4cout << "   Detectors:                                                                         " << G4endl;
+      G4cout << " # Physics setups:                                                                    " << G4endl;
+      G4cout << "   Elastic Scale Factor   : " << physicsList->elasticScaleFactor                        << G4endl; 
+      G4cout << "   Inelastic Scale Factor : " << physicsList->inelasticScaleFactor                      << G4endl;
+      G4cout << " # Primary sources:                                                                   " << G4endl;  
+      G4cout << " ------------------------------------------------------------------------------------ " << G4endl;
+      G4cout << " # Number of worker threads        : " << numberOfWorkerThreads << " threads"           << G4endl;
+      G4cout << " # Number of primary events        : " << nofEvents             << " events"            << G4endl;
+      G4cout << " # Number of total events (scaled) : " << 500*nofEvents         << " events"            << G4endl;
+      G4cout << " # Elapsed time " << (double(diffclock(end, begin)/1000)) << " s"                       << G4endl;
+      G4cout << "                                                                                      " << G4endl;
+      G4cout << "                                                                                      " << G4endl;
     }
+    else
+    {}
 
     // print histogram statistics
     //
